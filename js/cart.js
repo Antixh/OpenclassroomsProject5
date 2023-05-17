f177c7d23005cc66249f1e2d380d305486ff4314
Antoine Hamard <antoine.hamard@ausy.fr>
2023-05-17 15:16:33 +0200

    modification script page panier

--- js/cart.js
+++ js/cart.js
@@ -1,40 +1,44 @@
 // Récupération du localStorage
 const myLocalStorage = JSON.parse(window.localStorage.getItem("panier"));
-console.log(myLocalStorage);
 
-// Gestion des promesses
-const gestionPromesse = myLocalStorage.map(reference => {
-    return new Promise((resolve, reject) => {
-        // Appel à l'API
-        const idCanape = reference.id;
-        async function canapeAPI() {
-            const reponse = await fetch(`http://localhost:3000/api/products/${ idCanape }`);
-            if (reponse.ok === true) {
-                return reponse.json();
-            }
-            throw new Error("Erreur de connexion au serveur de l'API");
-        }
+// Appel à l'API
+async function produitsAPI() {
+    const reponse = await fetch("http://localhost:3000/api/products");
+    if (reponse.ok === true) {
+        return reponse.json();
+    }
+    throw new Error("Erreur de connexion au serveur de l'API");
+}
 
-        canapeAPI().then( canape => {
-            // Affichage liste des canapés dans le panier
-            let prix = canape.price * reference.quantite;
-            const listeArticles = document.getElementById("cart__items");
-            listeArticles.insertAdjacentHTML('afterbegin', 
-            `
-                <article class="cart__item" data-id="${ idCanape }" data-color="${ reference.couleur }">
+/*
+    Affichage des différents canapés du localStorage
+    Écouteurs d'événements pour la modification et la suppression d'un canapé
+*/
+async function constructionDOM() {
+    return produitsAPI()
+    .then(listeCanapes => {
+        const listeArticles = document.getElementById("cart__items");
+        // On crée une boucle pour parcourir le localStorage et afficher chacun des items
+        for (let i = 0; i < myLocalStorage.length; i++) {
+            const refIndex = listeCanapes.findIndex(item => item._id === myLocalStorage[i].id);
+            const refCanape = listeCanapes[refIndex];
+            const canapeStorage = myLocalStorage[i];
+            let monCanapPrix = refCanape.price * canapeStorage.quantite;
+            listeArticles.insertAdjacentHTML('afterbegin', `
+                <article class="cart__item" data-id="${canapeStorage.id}" data-color="${canapeStorage.couleur}">
                     <div class="cart__item__img">
-                        <img src="${ canape.imageUrl }" alt="Photographie d'un canapé">
+                        <img src="${refCanape.imageUrl}" alt="Photographie d'un canapé">
                     </div>
                     <div class="cart__item__content">
                         <div class="cart__item__content__description">
-                            <h2>${ canape.name }</h2>
-                            <p>${ reference.couleur }</p>
-                            <p class="prix_article">${ prix }€</p>
+                            <h2>${refCanape.name}</h2>
+                            <p>${canapeStorage.couleur}</p>
+                            <p class="prix_article">${monCanapPrix}€</p>
                         </div>
                         <div class="cart__item__content__settings">
                             <div class="cart__item__content__settings__quantity">
                                 <p>Qté : </p>
-                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${ reference.quantite }">
+                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${canapeStorage.quantite}">
                             </div>
                             <div class="cart__item__content__settings__delete">
                                 <p class="deleteItem">Supprimer</p>
@@ -44,40 +48,40 @@ const gestionPromesse = myLocalStorage.map(reference => {
                 </article>
             `);
 
-            // Supression de canapé(s) dans le panier
-            const supprimerCanape = document.querySelector(`article[data-id="${ idCanape }"] .deleteItem`);
-            supprimerCanape.addEventListener("click", () => {
-                console.log(`Supprimer canapé(s): ${canape.name}, id: ${canape._id}`);
-            });
-            
-            // Modification de la quantité de canapé(s) dans le panier
-            const modifQuantite = document.querySelector(`article[data-id="${ idCanape }"] .itemQuantity`);
+            // Catch de la modification de quantité
+            const modifQuantite = document.querySelector(`article[data-id="${canapeStorage.id}"] .itemQuantity`);
             modifQuantite.addEventListener("change", () => {
                 const nouvelleQuantite = Number(modifQuantite.value);
-                const filtreCanape = myLocalStorage.findIndex(obj => reference.id === obj.id && reference.couleur === obj.couleur);
+                const filtreCanape = myLocalStorage.findIndex(obj => canapeStorage.id === obj.id && canapeStorage.couleur === obj.couleur);
                 myLocalStorage[filtreCanape].quantite = nouvelleQuantite;
-                prix = nouvelleQuantite * canape.price;
+                monCanapPrix = nouvelleQuantite * refCanape.price;
                 const nouveauPanier = JSON.stringify(myLocalStorage);
                 window.localStorage.setItem("panier", nouveauPanier);
-                const prixArticle = document.querySelector(`article[data-id="${ idCanape }"]  .prix_article`);
-                prixArticle.innerHTML = `${prix}€`;
+                const prixArticle = document.querySelector(`article[data-id="${canapeStorage.id}"]  .prix_article`);
+                prixArticle.innerHTML = `${monCanapPrix}€`;
+
                 totalCanapesPanier();
                 prixTotal();
             });
-        });
-        console.log("Ok");
-        resolve();
-    })
-})
 
-// On attend que toutes les promesses soient résolues pour afficher le prix total
-Promise.all(gestionPromesse)
-    .then(() => {
-        prixTotal();
-    })
-    .catch(error => {
-        console.error(error);
-    })
+            // Catch de la suppression d'un canapé
+            const supprCanape = document.querySelector(`article[data-id="${canapeStorage.id}"] .deleteItem`);
+            supprCanape.addEventListener("click", () => {
+                myLocalStorage.splice(i, 1);
+                const nouveauPanier = JSON.stringify(myLocalStorage);
+                window.localStorage.setItem("panier", nouveauPanier);
+                console.log(myLocalStorage.length);
+                if(myLocalStorage.length === 0) {
+                    localStorage.removeItem("panier");
+                }
+                listeArticles.innerHTML = "";
+                totalCanapesPanier();
+                prixTotal();
+                constructionDOM();
+            })
+        }
+    });
+}
 
 // Affichage total canapés
 function totalCanapesPanier() {
@@ -90,12 +94,10 @@ function totalCanapesPanier() {
     totalQuantite.innerHTML = totalCanapes;
 }
 
-totalCanapesPanier();
-
+// Affichage prix total
 function prixTotal() {
     const getPrix = document.getElementsByClassName("prix_article");
     let tableauPrix = [];
-    console.log(getPrix);
 
     if (getPrix.length !== 0) {
         for (let i = 0; i < getPrix.length; i++) {
@@ -107,9 +109,38 @@ function prixTotal() {
         const prixTotalHTML = document.getElementById("totalPrice");
         prixTotalHTML.innerHTML = sommePrix;
     } else {
-        console.log("Panier vide");
-        const listeArticles = document.getElementById("cart__items");
-        listeArticles.insertAdjacentHTML('afterbegin',
-        `<p>Votre panier est vide, vous pouvez trouver l'ensemble de nos produits en suivant ce <a href="./index.html">lien</a>.</p>`);
+        const prixTotalHTML = document.getElementById("totalPrice");
+        prixTotalHTML.innerHTML = 0;
+        panierVide();
     }
 }
+
+// Gestion du panier vide
+function panierVide() {
+    const listeArticles = document.getElementById("cart__items");
+        listeArticles.insertAdjacentHTML('afterbegin',
+        `<p>Votre panier est vide, vous pouvez trouver l'ensemble de nos produits en suivant ce <a href="./index.html">lien</a>.</p>`);
+        const infoPanier = document.querySelectorAll(".cart__price");
+        for(const el of infoPanier) {
+            el.parentNode.removeChild(el);
+        }
+}
+
+/*
+    On attend que l'appel d'API soit terminé pour construire la page et afficher les valeurs 
+    Quantité totale de canapé(s)
+    Prix total du panier
+    Si le panier est vide on met un lien vers la page d'accueil
+*/
+function pagePanier() {
+    constructionDOM()
+    .then(() => {
+        totalCanapesPanier();
+        prixTotal();
+    })
+    .catch(error => {
+        panierVide();
+    });
+}
+
+pagePanier();
